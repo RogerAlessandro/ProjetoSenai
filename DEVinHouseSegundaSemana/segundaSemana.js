0d79704875d4cf9c795c9cf2c484b8b36c94e3d4
RogerAlessandro <108702351+RogerAlessandro@users.noreply.github.com>
2022-07-11 10:48:27 -0300

    [M1S02] Ex 2 - Prompt + document.write
    Agora levando em conta o exercício 01, também capturando o que o usuário digita com o prompt, faça-o digitar o seu nome em um prompt e o email dele em outro prompt, em seguida imprima em uma tag html h4 as duas informações digitadas pelo usuário.

---
+++ DEVinHouseSegundaSemana/segundaSemana.js
@@ -0,0 +1,9 @@
+var nome;
+var email;
+nome = window.prompt("Digite seu nome.");
+document.getElementById("nome").innerHTML = nome;
+//document.write (nome);
+email = window.prompt("Digite seu email.")
+document.getElementById("email").innerHTML = email;
+//document.write("<br>"+email);
+
