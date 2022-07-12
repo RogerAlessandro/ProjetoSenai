83343e799657dbd79ea94ba217ab7bfc00685de3
RogerAlessandro <108702351+RogerAlessandro@users.noreply.github.com>
2022-07-12 14:31:22 -0300

    [M1S02] Ex 8 - Calculadora
    Editar
    Utilizando estrutura de repetição for realize a tabuada do número 5 e exibe em tela da seguinte forma:
    
    5 x 0 = 0
    5 x 1 = 5
    5 x 2 = 10
    ....
    5 x 10 = 50

--- DEVinHouseSegundaSemana/segundaSemana.js
+++ DEVinHouseSegundaSemana/segundaSemana.js
@@ -1,22 +1,29 @@
-var contador = 0, resposta, qtdPessoas = 0;
-while (contador < 4) {
-    resposta = window.prompt("Qual seria sua classificação para a série stranger things? ");
+// var contador = 0, resposta, qtdPessoas = 0;
+// while (contador < 4) {
+//     resposta = window.prompt("Qual seria sua classificação para a série stranger things? ");
 
-    switch (resposta) {
-        case "ruim": qtdPessoas++;
-            break;
-        case "bom":
-            break;
-        case "excelente":
-            break;
+//     switch (resposta) {
+//         case "ruim": qtdPessoas++;
+//             break;
+//         case "bom":
+//             break;
+//         case "excelente":
+//             break;
 
-        default: resposta = window.prompt("Opções válidas são apenas Bom, Ruim ou Exelente. \n Digite novamente sua avaliação. ");
-            if (resposta == "ruim") {
-                qtdPessoas++;
-            }
+//         default: resposta = window.prompt("Opções válidas são apenas Bom, Ruim ou Exelente. \n Digite novamente sua avaliação. ");
+//             if (resposta == "ruim") {
+//                 qtdPessoas++;
+//             }
 
-    }
-    contador++;
+//     }
+//     contador++;
+// }
+
+// document.getElementById("resultado").innerHTML = '<h3>Quantidade de pessoas que classificou como ruim foi: ' + qtdPessoas + '</h3>'
+
+
+for (let i = 1; i <= 10; i ++) {
+    let result = 5 * i;
+    console.log(result);
+    document.write("<p> 5 x", i, "=", result, "</p>" );
 }
-
-document.getElementById("resultado").innerHTML = '<h3>Quantidade de pessoas que classificou como ruim foi: ' + qtdPessoas + '</h3>'
