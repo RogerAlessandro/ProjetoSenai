42657be6e04333e1b8434c23e39a2f9c95a4418d
RogerAlessandro <108702351+RogerAlessandro@users.noreply.github.com>
2022-07-12 10:17:03 -0300

    [M1S02] Ex 6 - IMC
    Com a ajuda da estrutura condicional crie um código que irá capturar o peso do usuário, após isso calcule o imc tomando a base o cálculo IMC = Peso ÷ (Altura × Altura). 
    Caso o valor digitado sejá maior ou igual a 25 apresente em um alert a mensagem, “Você está acima do peso, procure um médico”, caso seja entre 18,5 e 24,9 imprimir em tela, na própria página html a mensagem "PARABÉNS!! Você está no peso ideal” e caso não seja nenhuma das condições acima, imprima também em tela “Você está abaixo do peso, procure um médico.”

--- DEVinHouseSegundaSemana/segundaSemana.js
+++ DEVinHouseSegundaSemana/segundaSemana.js
@@ -1,18 +1,15 @@
-
-
-
-var peso = parseFloat(prompt("Inorme seu peso."));
+var peso = parseFloat(prompt("Informe seu peso. "));
 console.log(peso);
-var altura = parseFloat(prompt("Informe sua altura. "))
+var altura = parseFloat(prompt("Informe sua altura. "));
 console.log(altura);
 var imc = peso / altura **2;
 console.log(imc);
 if (imc > 18.55 && imc < 24.99){
-    alert("Parabéns! Mantenha-se assim.");
-}else if (imc >= 25) {
-    alert("Parabéns, você foi aprovado para o programa Quilos Mortais.");
+    alert("Parabéns! Você esta no peso ideal. ");
+}else if(imc >= 25){
+alert("Você esta acima do peso. ");
 } else {
-    alert("Magrelo! Vá se alimentar, academia e tomar Bomba!")
+    alert("Você esta abaixo do peso, procure um médico. ")
 }
 
 
