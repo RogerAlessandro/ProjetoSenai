da9fe2675ee98a05bfef2a585faf28229cb0341e
RogerAlessandro <108702351+RogerAlessandro@users.noreply.github.com>
2022-07-14 14:38:32 -0300

    [M1S02] Ex 9 - Mercadinho do dev
    Você foi contratado por um supermercado para catalogar a compra de uma cliente. Assim que a aplicação começa deverá ser perguntado em um prompt qual produto o cliente deseja comprar, só serão aceitas as seguintes respostas:
    
    (1) Hortifruti
    (2) Laticínios
    (3) Carnes
    (4) Peixes
    (5) Aves
    (6) Fechar pedido
    Após fazer a seleção do produto pergunte a quantidade de itens que o cliente irá comprar.
    Apenas se o cliente selecionar a opção 6 é que a pergunta deverá parar de ser repetida.
    Ao selecionar a opção 6 imprima o produto que o cliente pegou em maior quantidade.

--- DEVinHouseSegundaSemana/segundaSemana.js
+++ DEVinHouseSegundaSemana/segundaSemana.js
@@ -15,6 +15,14 @@
 //                 qtdPessoas++;
 //             }
 
+//             document.getElementById("resultado").innerHTML = '<h3>Quantidade de pessoas que classificou como ruim foi: ' + qtdPessoas + '</h3>'
+
+
+//             for (let i = 1; i <= 10; i++) {
+//                 let result = 5 * i;
+//                 console.log(result);
+//                 document.write("<p> 5 x", i, "=", result, "</p>");
+//             }
 //     }
 //     contador++;
 // }
@@ -22,8 +30,47 @@
 // document.getElementById("resultado").innerHTML = '<h3>Quantidade de pessoas que classificou como ruim foi: ' + qtdPessoas + '</h3>'
 
 
-for (let i = 1; i <= 10; i ++) {
-    let result = 5 * i;
-    console.log(result);
-    document.write("<p> 5 x", i, "=", result, "</p>" );
+// for (let i = 1; i <= 10; i++) {
+//     let result = 5 * i;
+//     console.log(result);
+//     document.write("<p> 5 x", i, "=", result, "</p>");
+// }
+
+var produtos = ['Hortifruti', 'Laticinios', 'Carnes', 'Peixes', 'Aves', 'Finaliza Pedido'];
+var qtd = [0, 0, 0, 0, 0];
+var finaliza = false;
+var opcao = "";
+
+while (finaliza == false) {
+    opcao = window.prompt("Olá, qual dos nossos produtos deseja comprar: 1-Hortifruti, 2-Laticinios, 3-Carnes, 4-Peixes, 5-Aves, 6-Finaliza Pedido");
+    
+    if (parseInt(opcao) < 1 || parseInt(opcao) > 6) {
+        opcao = window.prompt("Favor inserir os numerais de 1 a 5 para e produtos e 6 para finalizar a venda");
+    } else {
+        switch (opcao) {
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+                var i = parseInt(opcao) - 1;
+                var qt = 0;
+                qt = window.prompt("Quantos " + produtos[i] + " você deseja comprar?");
+                qtd[i] = qtd[i] + parseInt(qt);
+                break;
+            case '6':
+                window.alert("Obrigado pelas compras, volte sempre. ");
+                finaliza = true;
+                break;
+        }
+    }
 }
+document.write("<br /> Os item comprados foram: ")
+
+for (i = 0; i <= qtd.length - 1; i++) {
+    document.write("<br />" + produtos[i] + " - " + qtd[i] + " itens ")
+}
+const qtd_maior = Math.max.apply(0, qtd);
+const prod_maior = qtd.indexOf(qtd_maior);
+document.write("<p>O item " + produtos[prod_maior] + " Foi o mais solicitado com " + qtd_maior + " itens.</p>");
+
