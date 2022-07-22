1983700803949c830d45854416f4d8774fc99344
supervisor <roger.cunha@clamed.com.br>
2022-07-22 13:52:36 -0300

    Exe4Semana3

---
+++ TerceiraSemana/[M1S03] Ex 04 - DOM/index.js
@@ -0,0 +1,4 @@
+// Crie um site (arquivo HTML) que, ao ser renderizado, adicione (arquivo JS)
+//  5 textos quaisquer, um ao lado outro, com a cor azul (arquivo css).
+
+var texto = document.getElementById('texto').innerHTML = ' -Cachoro- ' + " -Moto- " + " -Viajem- " + " -Futebol Americano- " + " -Lutas ";
