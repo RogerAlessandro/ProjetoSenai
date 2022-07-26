66a978689fbd5a9dd65394eb469b6ddca28e220c
supervisor <roger.cunha@clamed.com.br>
2022-07-26 10:54:49 -0300

    Ex8Semana3

---
+++ QuartaSemana/[M1S04] Ex 3 - Classe + instância/index.js
@@ -0,0 +1,19 @@
+// Crie uma classe chamada Pessoa com os atributos nome, idade, cpf e anoAniversario. 
+// Agora construa um método chamado podeDirigir dentro dessa classe para verificar se a idade é maior ou menor que 18, 
+// se for maior ou igual a 18 o retorno desse método será true se não false. Agora, fora dessa classe crie uma instância (usando o new ) 
+// inicialize ela (usando o acesso a um atributo " . ") para uma variável const e verifique o retorno com os seus dados em seus atributo
+var dadosUsuario = ["nome", 'idade', 'cpf'];
+var podeDirigir = true;
+let idade = '';
+
+
+dadosUsuario = window.prompt(" Informe o nome ");
+dadosUsuario = parseInt(window.prompt(" Informe a idade "));
+dadosUsuario = parseInt(window.prompt(" Informe o CPF "));
+
+if (idade == podeDirigir) {
+    idade >= 18 
+    
+
+}
+
